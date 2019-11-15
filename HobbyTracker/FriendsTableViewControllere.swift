5fdb2eafb1f9b1680d62c7b75ccb3d8dbc124389
DavidSchwinne <davidschwinne@gmail.com>
2019-11-15 13:47:10 +0900

    Completed

---
+++ HobbyTracker/FriendsTableViewControllere.swift
@@ -0,0 +1,20 @@
+//
+  FriendsTableViewControllere.swift
+//  HobbyTracker
+//
+//  Created by LaFleur on 11/15/19.
+//  Copyright © 2019 David Schwinne. All rights reserved.
+//
+
+import UIKit
+
+class FriendsTableViewControllere: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
